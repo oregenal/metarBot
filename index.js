0d16a3f686e9e28e4b6337a1ffc0ccb051af991c
Andrey Gud <nosacred@gmail.com>
2020-08-17 20:36:44 +0700

    express

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-// const { Telegraf } = require('telegraf')
-const { Composer } = require('micro-bot')
+const { Telegraf } = require('telegraf')
+const express = require('express')
+// const { Composer } = require('micro-bot')
 const fetch = require('node-fetch')
+
+const expressApp = express()
 require('dotenv').config()
 
 const url = 'https://tgftp.nws.noaa.gov/data/observations/metar/stations/'
 
-// bot = new Telegraf(process.env.BOT_TOKEN)
-bot = new Composer
+const port = process.env.PORT || 3000
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+expressApp.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+bot = new Telegraf(process.env.BOT_TOKEN)
+// bot = new Composer
 bot.start(ctx => ctx.reply('Welcome'))
 bot.help(ctx => ctx.reply('Send me a sticker'))
 bot.on('sticker', ctx => ctx.reply('👍'))
@@ -17,8 +28,8 @@ bot.hears('unkl', ctx => {
     .then(Response => Response.text())
     .then(Response => ctx.reply(Response))
 })
-// bot.launch()
-module.exports = bot
+bot.launch()
+// module.exports = bot
 
 const getMetar = (icao) => {
   fetch(url + icao.toUpperCase() + '.TXT')
