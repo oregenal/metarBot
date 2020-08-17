90b35e78a9d2ba40c794aef58ca398827db96cc0
Andrey Gud <nosacred@gmail.com>
2020-08-17 22:41:15 +0700

    v1.0.2 Case Non Sensetive

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ expressApp.listen(port, () => {
 })
 
 bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start(ctx => ctx.reply('Welcome'))
+bot.start(ctx => ctx.reply('Бот Жора предсказывает погоду.'))
 bot.help(ctx => ctx.reply('Пишите: metar icao - для получения метара'))
 bot.on('sticker', ctx => ctx.reply('✈️'))
-bot.hears('hi', ctx => ctx.reply('Hey there'))
-bot.hears(/^metar/, ctx => {
+bot.hears(/[H,h]i/, ctx => ctx.reply('Hey there'))
+bot.hears(/^[M,m]etar/, ctx => {
   fetch(url + ctx.message.text.slice(6, 10).toUpperCase() + '.TXT')
     .then(Response => Response.text())
     .then(Response => Response.match(/404 Not Found/) ? ctx.reply('Произошла ошибка.') : ctx.reply(Response))
