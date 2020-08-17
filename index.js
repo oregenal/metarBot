3afeea7eccc52775dd2ddbc2de1f30ebe9825365
Andrey Gud <nosacred@gmail.com>
2020-08-17 18:14:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Telegraf } = require('telegraf')
+require('dotenv').config()
+
+const url = 'https://tgftp.nws.noaa.gov/data/observations/metar/stations/'
+
+
+bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('metar', (ctx) => ctx.reply('Work in progress...'))
+bot.launch()
+
+function getMetar(icao) {
+  fetch(url + icao.toUpperCase() + '.TXT')
+    .then(res => res.text())
+    .then(text => {
+      metarData.innerHTML = ''
+      const content = document.createElement('p')
+      content.innerHTML = text
+      metarData.appendChild(content)
+    })
+}
