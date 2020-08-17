0a5ead53a77e5d19dd7222512216fb4ef6625e6d
Andrey Gud <nosacred@gmail.com>
2020-08-17 19:15:32 +0700

    unkl hardCode

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ let metar = ''
 bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears('metar', (ctx) => ctx.reply('Work in progress...'))
-bot.hears('unkl', (ctx) => {
+bot.on('sticker', ctx => ctx.reply('👍'))
+bot.hears('hi', ctx => ctx.reply('Hey there'))
+bot.hears('metar', ctx => ctx.reply('Work in progress...'))
+bot.hears('unkl', ctx => {
   fetch(url + 'UNKL.TXT')
     .then(Response => Response.text())
     .then(Response => ctx.reply(Response))
