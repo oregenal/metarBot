41dfe0d215b8eb3ec343f2a19f7206e9b6e06ee1
Andrey Gud <nosacred@gmail.com>
2020-08-17 19:17:13 +0700

    hardCode UNKL

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require('dotenv').config()
 const url = 'https://tgftp.nws.noaa.gov/data/observations/metar/stations/'
 
 bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.start(ctx => ctx.reply('Welcome'))
+bot.help(ctx => ctx.reply('Send me a sticker'))
 bot.on('sticker', ctx => ctx.reply('👍'))
 bot.hears('hi', ctx => ctx.reply('Hey there'))
 bot.hears('metar', ctx => ctx.reply('Work in progress...'))
